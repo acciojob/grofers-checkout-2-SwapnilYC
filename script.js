20ca8e54fbc957fca4bb05afa61869caf7387d76
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-20 07:22:53 +0530

    code added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+let tableEl = document.getElementById('myTable');
+let priceArr = Array.from(document.querySelectorAll("td[data-ns-test]"));
+// console.log(priceArr);
+let totalPrice = 0;
+for (const priceOfEachItem of priceArr) {
+  totalPrice += Number(priceOfEachItem.textContent);
+}
+
+let totalRow = document.createElement('tr');
+let newcell = totalRow.insertCell(0);
+newcell.textContent = totalPrice
+
+tableEl.insertAdjacentElement('beforeend', totalRow);
